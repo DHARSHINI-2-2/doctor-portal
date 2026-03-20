5fd7eb1ee44c1efd74d967a7bb18b652a9fb288e
DHARSHINI-2-2 <smdharshini22@gmail.com>
2026-03-20 16:51:19 +0530

    updated api.js storage problem

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,11 +6,13 @@ const api = axios.create({
 
 // Automatically attach the JWT token to every request if it exists
 api.interceptors.request.use((config) => {
-    const user = JSON.parse(localStorage.getItem('user'));
+    const user = JSON.parse(sessionStorage.getItem('user'));
     if (user && user.token) {
         config.headers.Authorization = `Bearer ${user.token}`;
     }
     return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default api;
